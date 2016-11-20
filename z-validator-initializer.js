0c722f617a3fae5c3b8262235b899a6bbc8a03ed
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-11-20 13:44:08 +0400

    removed camelCase path for z-validator rules

--- z-validator-initializer.js
+++ z-validator-initializer.js
@@ -4,6 +4,7 @@ module.exports = {
     run: function (app, next) {
 
         var validate = require("validate.js");
+        var fs = require('fs');
 
         app.validator = {
             buildErrorObject: function (errorArray) {
@@ -19,6 +20,12 @@ module.exports = {
             },
             validate: function (data, type) {
                 var fn = function (req, res, next) {
+
+                    if(!data){
+                        var validatorPath = req.originalUrl.replace('/', '').replace(/\/$/, '');
+                        validatorPath = validatorPath.replace(/\//g, '.');
+                        data = app._.get(app.validator, validatorPath);
+                    }
 
                     if (app.config.zValidator.log) {
                         app.logger.info("VALIDATE INCOMING PARAMS", req.body);
@@ -41,7 +48,31 @@ module.exports = {
             }
         };
 
-        app.utils.buildModulesInFolder(app, app.validator, app.folderPath.app.root + app.config.zValidator.rootDir + '/');
+        function buildModulesInFolder (app, namespace, dir) {
+            if (fs.existsSync(dir)) {
+                var rootDir = fs.readdirSync(dir);
+
+                if (rootDir && rootDir.length > 0) {
+                    rootDir.forEach(function (file) {
+                        var nameParts = file.split('/');
+                        var name = nameParts[(nameParts.length - 1)].split(".")[0];
+                        var filePath = dir + file;
+
+                        if (fs.lstatSync(filePath).isDirectory()) {
+                            namespace[name] = {};
+                            return buildModulesInFolder(app, namespace[name], filePath + '/');
+                        } else {
+                            if (fs.existsSync(filePath)) {
+                                var module = require(filePath);
+                                namespace[name] = new module(app);
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        buildModulesInFolder(app, app.validator, app.folderPath.app.root + app.config.zValidator.rootDir + '/');
 
         next();
     }
