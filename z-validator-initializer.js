e277736c41a71b114127d996677df947b8aab820
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-11-20 17:40:28 +0400

    refactored error response object

--- z-validator-initializer.js
+++ z-validator-initializer.js
@@ -19,36 +19,32 @@ module.exports = {
                 }
             },
             validate: function (data, type) {
-                var fn = function (req, res, next) {
+                return function (req, res, next) {
+                    var parameters = app._.merge(req.params, req.body, req.query);
 
-                    if(!data){
+                    if (!data) {
                         var validatorPath = req.originalUrl.replace('/', '').replace(/\/$/, '');
                         validatorPath = validatorPath.replace(/\//g, '.');
                         data = app._.get(app.validator, validatorPath);
                     }
 
                     if (app.config.zValidator.log) {
-                        app.logger.info("VALIDATE INCOMING PARAMS", req.body);
+                        app.logger.info("VALIDATE INCOMING PARAMS", parameters);
                     }
 
-                    var errors = validate(req.body, data, {format: app.config.zValidator.format || "flat"});
+                    var errors = validate(parameters, data, {format: "grouped"});
 
-                    if (app.config.zValidator.useZFormat) {
-                        errors = app.validator.buildErrorObject(errors);
-                    }
-
-                    if (app.config.zValidator.log && errors && errors.length > 0) {
+                    if (app.config.zValidator.log && errors) {
                         app.logger.info("VALIDATION FAILED", errors);
-                        throw {errors: errors};
+                        throw castErrors(errors);
                     }
 
                     next();
                 };
-                return fn;
             }
         };
 
-        function buildModulesInFolder (app, namespace, dir) {
+        function buildModulesInFolder(app, namespace, dir) {
             if (fs.existsSync(dir)) {
                 var rootDir = fs.readdirSync(dir);
 
@@ -72,9 +68,24 @@ module.exports = {
             }
         }
 
+        function castErrors(errorObj) {
+            var errors = [];
+            for (var i in errorObj) {
+                var currErrorObj = errorObj[i];
+                for (var j in currErrorObj) {
+                    var error = currErrorObj[j];
+                    errors.push({
+                        keyword: error,
+                        field: i
+                    })
+                }
+            }
+            return errors;
+        }
+
         buildModulesInFolder(app, app.validator, app.folderPath.app.root + app.config.zValidator.rootDir + '/');
 
         next();
     }
-};
 
+};
