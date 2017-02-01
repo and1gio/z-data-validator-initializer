ff6754ca15e5379b989ed57e1dd74c4356d71922
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2017-02-01 19:56:21 +0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # z-data-validator-initializer
+
+rules example - using [validate.js](https://validatejs.org/)
+
+**validators/api/example/hello/js**
+```javascript
+module.exports = function (app) {
+    return {
+        body: {
+            "data": {
+                presence: {
+                    message: "^REQUIRED"
+                }
+            },
+            "data.name": {
+                presence: {
+                    message: "^REQUIRED"
+                },
+                inclusion: {
+                    within : ['VALUE1', 'VALUE2'],
+                    message: "^INVALID_VALUE"
+                }
+            }
+        },
+        params: {
+            "id": {
+                presence: {
+                    message: "^REQUIRED"
+                }
+            },
+            "type": {
+                presence: {
+                    message: "^REQUIRED"
+                }
+            }
+        },
+        query: {
+            "orderBy": {
+                presence: {
+                    message: "^REQUIRED"
+                }
+            }
+        }
+    };
+};
+```
+
+**routes/api/example.js**
+```javascript
+'use strict';
+
+module.exports = function (app) {
+
+    var router = require('express').Router();
+
+    router.post(
+        '/hello/:id/:type',
+        app.zValidator.validate(app.zValidator.api.example.hello),
+        function (req, res, next) {
+            app.service.example.hello(req, function (error, data) {
+                error ? next(error) : res.json(data);
+            });
+        });
+
+    return router;
+};
+```
--- z-validator-initializer.js
+++ z-validator-initializer.js
@@ -4,39 +4,43 @@ module.exports = {
     run: function (app, next) {
 
         var validate = require("validate.js");
-        var fs = require('fs');
 
-        app.validator = {
-            buildErrorObject: function (errorArray) {
-                if (errorArray && errorArray.length > 0) {
-                    var errors = [];
-                    for (var i in errorArray) {
-                        errors.push({keyword: errorArray[i]});
-                    }
-                    return errors;
-                } else {
-                    return null;
-                }
-            },
-            validate: function (data, type) {
+        app.zValidator = {
+            validate: function (rule) {
                 return function (req, res, next) {
-                    var parameters = app._.merge(req.params, req.body, req.query);
-
-                    if (!data) {
+                    if (!rule) {
                         var validatorPath = req.originalUrl.replace('/', '').replace(/\/$/, '');
                         validatorPath = validatorPath.replace(/\//g, '.');
-                        data = app._.get(app.validator, validatorPath);
+                        rule = app._.get(app.zValidator, validatorPath);
+                    }
+
+                    if(!rule){
+                        app.logger.warn("VALIDATION RULE NOT FOUND", validatorPath);
+                        return next();
                     }
 
                     if (app.config.zValidator.log) {
-                        app.logger.info("VALIDATE INCOMING PARAMS", parameters);
+                        app.logger.info("VALIDATE INCOMING REQUEST BODY", req.body);
+                        app.logger.info("VALIDATE INCOMING QUERY", req.query);
+                        app.logger.info("VALIDATE INCOMING PARAMS ", req.params);
                     }
 
-                    var errors = validate(parameters, data, {format: "grouped"});
+                    var bodyErrors = validate(req.body, rule.body, {format: "grouped"});
+                    var queryErrors = validate(req.query, rule.query, {format: "grouped"});
+                    var paramsErrors = validate(req.params, rule.params, {format: "grouped"});
+
+                    var errors = app._.concat(bodyErrors, queryErrors, paramsErrors);
+
+                    if (app.config.zValidator.useZFormat) {
+                        errors = clearErrors (errors);
+                    }
 
                     if (app.config.zValidator.log && errors) {
                         app.logger.info("VALIDATION FAILED", errors);
-                        throw castErrors(errors);
+                    }
+
+                    if(errors){
+                        throw {errors: errors};
                     }
 
                     next();
@@ -44,48 +48,22 @@ module.exports = {
             }
         };
 
-        function buildModulesInFolder(app, namespace, dir) {
-            if (fs.existsSync(dir)) {
-                var rootDir = fs.readdirSync(dir);
-
-                if (rootDir && rootDir.length > 0) {
-                    rootDir.forEach(function (file) {
-                        var nameParts = file.split('/');
-                        var name = nameParts[(nameParts.length - 1)].split(".")[0];
-                        var filePath = dir + file;
-
-                        if (fs.lstatSync(filePath).isDirectory()) {
-                            namespace[name] = {};
-                            return buildModulesInFolder(app, namespace[name], filePath + '/');
-                        } else {
-                            if (fs.existsSync(filePath)) {
-                                var module = require(filePath);
-                                namespace[name] = new module(app);
-                            }
-                        }
-                    });
+        function clearErrors (errorArray) {
+            if (errorArray && errorArray.length > 0) {
+                var errors = [];
+                for (var i in errorArray) {
+                    if(errorArray[i]){
+                        errors.push(errorArray[i]);
+                    }
                 }
+                return errors;
+            } else {
+                return null;
             }
         }
 
-        function castErrors(errorObj) {
-            var errors = [];
-            for (var i in errorObj) {
-                var currErrorObj = errorObj[i];
-                for (var j in currErrorObj) {
-                    var error = currErrorObj[j];
-                    errors.push({
-                        keyword: error,
-                        field: i
-                    })
-                }
-            }
-            return errors;
-        }
-
-        buildModulesInFolder(app, app.validator, app.folderPath.app.root + app.config.zValidator.rootDir + '/');
+        app.utils.buildModulesInFolder(app, app.zValidator, app.folderPath.app.root + app.config.zValidator.rootDir + '/');
 
         next();
     }
-
 };
