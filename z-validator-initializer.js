c83a75795f9a9b19d2c3d98927679dba7af15034
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2017-02-23 16:10:57 +0400

    nVersion

--- z-validator-initializer.js
+++ z-validator-initializer.js
@@ -6,7 +6,40 @@ module.exports = {
         var validate = require("validate.js");
 
         app.zValidator = {
-            validate: function (rule) {
+            validate: function (requestBody, requestParams, requestQuery, rule) {
+
+                if (!rule) {
+                    app.logger.warn("VALIDATION RULE NOT FOUND");
+                    throw [{"keyword": "VALIDATION_RULE_NOT_FOUND"}];
+                }
+
+                if (app.config.zValidator.log) {
+                    app.logger.info("VALIDATE INCOMING REQUEST BODY", requestBody);
+                    app.logger.info("VALIDATE INCOMING PARAMS ", requestParams);
+                    app.logger.info("VALIDATE INCOMING QUERY", requestQuery);
+                }
+
+                var errors = [];
+
+                var bodyErrors = validate(requestBody, rule.body, {format: "grouped"});
+                buildErrors(errors, bodyErrors, "requestBody");
+
+                var paramsErrors = validate(requestParams, rule.params, {format: "grouped"});
+                buildErrors(errors, paramsErrors, "params");
+
+                var queryErrors = validate(requestQuery, rule.query, {format: "grouped"});
+                buildErrors(errors, queryErrors, "query");
+
+                if (app.config.zValidator.log && errors && errors.length > 0) {
+                    app.logger.info("VALIDATION FAILED", errors);
+                }
+
+                if (errors && errors.length > 0) {
+                    throw {errors: errors};
+                }
+            },
+
+            check: function (rule) {
                 return function (req, res, next) {
                     if (!rule) {
                         var validatorPath = req.originalUrl.replace('/', '').replace(/\/$/, '');
@@ -14,44 +47,15 @@ module.exports = {
                         rule = app._.get(app.zValidator, validatorPath);
                     }
 
-                    if(!rule){
-                        app.logger.warn("VALIDATION RULE NOT FOUND", validatorPath);
-                        return next();
-                    }
-
-                    if (app.config.zValidator.log) {
-                        app.logger.info("VALIDATE INCOMING REQUEST BODY", req.body);
-                        app.logger.info("VALIDATE INCOMING QUERY", req.query);
-                        app.logger.info("VALIDATE INCOMING PARAMS ", req.params);
-                    }
-
-                    var errors = [];
-
-                    var bodyErrors = validate(req.body, rule.body, {format: "grouped"});
-                    buildErrors (errors, bodyErrors, "requestBody");
-
-
-                    var queryErrors = validate(req.query, rule.query, {format: "grouped"});
-                    buildErrors (errors, queryErrors, "query");
-
-                    var paramsErrors = validate(req.params, rule.params, {format: "grouped"});
-                    buildErrors (errors, paramsErrors, "params");
-
-                    if (app.config.zValidator.log && errors && errors.length > 0) {
-                        app.logger.info("VALIDATION FAILED", errors);
-                    }
-
-                    if(errors && errors.length > 0){
-                        throw {errors: errors};
-                    }
+                    app.zValidator.validate(req.body, req.params, req.query, rule);
 
                     next();
                 };
             }
         };
 
-        function buildErrors (result, validatedResult, type) {
-            if(validatedResult){
+        function buildErrors(result, validatedResult, type) {
+            if (validatedResult) {
                 for (var key in validatedResult) {
                     result.push({
                         from: type,
